e620e8a34a6a11a1c2d783f6cb56f5444a7b0a5e
Ralph Grishman <grishman@proteus100.cs.nyu.edu>
2016-01-14 13:53:53 -0500

    Incorporate Yifan's 'fromLine' method into AnchoredPath (used by AnchoredPathSet).

--- src/AceJet/AnchoredPathSet.java
+++ src/AceJet/AnchoredPathSet.java
@@ -14,7 +14,10 @@ public class AnchoredPathSet {
 		String line;
 		int count = 0;
 		while ((line = reader.readLine()) != null) {
-			AnchoredPath p = new AnchoredPath(line);
+			AnchoredPath p = new AnchoredPath.fromLine(line);
+			if (p == null) {
+				continue;
+			}
 			paths.add(p);
 			if (pathIndex.get(p.path) == null)
 				pathIndex.put(p.path, new ArrayList<AnchoredPath>());
